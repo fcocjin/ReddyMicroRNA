ba1ea067fb4f20aa043c594a132c18a31ce7f75e
fcocjin <fcocjin@luc.edu>
2016-03-21 16:00:38 -0500

    This should work for BLASTing a single microRNA. I still need to work on how to work with multiple BLAST records and adjust the labeling of each result accordingly

--- BLAST_Test.py
+++ BLAST_Test.py
@@ -1,37 +1,48 @@
-#This program just takes in a file that I have on my computer so that I could practice BLASTing from python.
-#I know that I will need to adjust this when we make our own database, but I
-#wanted to get started on practicing the code even before the database was constructed
-from Bio.Blast import NCBIWWW #Used to access BLAST online
-from Bio.Blast import NCBIXML #Used to parse the BLAST output
-from Bio import SeqIO #Used to read the fasta file provided
-record = SeqIO.read("pmrA_Test.fasta", format = "fasta")
-print("record = " + record) #Made sure the record was correct
-result_Handle = NCBIWWW.qblast("blastn", "nt", record.seq) #BLASTs against the nt database
-##writer = open("pmrA_Results.txt", "w") #I will practice writing to file later
-print("Here!") #Marks that the BLAST has been performed
-blast_record = NCBIXML.read(result_Handle)
-E_VALUE_THRESH = 0.04 #Copied the next lines from the BioPython cookbook for practice
-for alignment in blast_record.alignments:
-    for hsp in alignment.hsps:
-        if hsp.expect < E_VALUE_THRESH:
-            print('****Alignment****')
-            s = alignment.title
-            l = s.split("|")
-            s = l[4].rstrip()
-            l = s.split(" ")
-            s = l[0] + ' ' + l[1] + ' ' + l[2] #????
-            print('Organism:', s)
-            print('length:', alignment.length)
-            print('e value:', hsp.expect)
-            print(hsp.query[0:75])
-            print(hsp.match[0:75])
-            print(hsp.sbjct[0:75])
-            print('query start:', hsp.query_start) #These lines are really what Reddy wants
-            print('query end:', hsp.query_end)
-            print('subject_start:', hsp.sbjct_start)
-            print('subject_end:', hsp.sbjct_end)
-            break #I only wanted to test against one BLAST result so I broke out of the provided loop
-        break
-    break
+# This program just takes in a file that I have on my computer so that I could practice BLASTing from python.
+# I know that I will need to adjust this when we make our own database, but I
+# wanted to get started on practicing the code even before the database was constructed
+# from Bio.Blast import NCBIWWW  # Used to access BLAST online
+# from Bio import SeqIO  # Used to read the fasta file provided
 
-print("Finished!")
+from Bio.Blast import NCBIXML  # Used to parse the BLAST output
+import os
+
+
+def parsefile(blast_record):
+    # record = SeqIO.read("pmrA_Test.fasta", format="fasta")
+    # print("record = " + record)  # Made sure the record was correct
+    # result_Handle = NCBIWWW.qblast("blastn", "nt", record.seq)  # BLASTs against the nt database
+    # writer = open("pmrA_Results.txt", "w") #I will practice writing to file later
+    # print("Here!")  # Marks that the BLAST has been performed
+    # blast_record = NCBIXML.read(result_Handle)
+
+    alignment = blast_record.alignments[0]
+    filename = input('what is your desired file name for the top hits file?')
+    writer = open(filename, 'w')
+    writer.write('Organism_name' + '\t' + 'Query_start' + '\t' + 'Query_end' + '\t' + 'Subject_start' + '\t' + 'Subject_end')
+    for hsp in alignment.hsps:
+        # Add a way to label each result with the corresponding microRNA
+        # Add a way to compensate for an empty result (no matches)
+        s = alignment.title
+        l = s.split("|")
+        s = l[4].rstrip()
+        l = s.split(" ")
+        s = l[0] + ' ' + l[1] + ' ' + l[2]  # ???? This was including a weird 'u'
+        writer.write(s + '\t' + hsp.query_start + '\t' + hsp.query_end + '\t' + hsp.sbjct_start + '\t' + hsp.sbjct_end )
+    print("Finished!")
+
+
+def main():
+    database = input('enter path to local blast database')  # input blast database to blast query against
+    query = input('enter the path to the query fasta file')  # input query fasta file (in our case, the miRNA)
+    output = input('what is your desired output path?')  # where should results go? output as text file
+    filename = input('what is your desired file name for the BLAST results? *please make it a .XML*')
+    run = "blastn -db " + database + "-query " + query + "-out " + output  # how run would appear in commandline
+    os.chdir(output)
+    result_handle = open(filename)
+    os.system(run)  # run through commandline (Saves the blast record)
+    blast_record = NCBIXML.read(result_handle)
+    parsefile(blast_record)
+# Still need to work on iterating through each microRNA and how to organize each of those (Perhaps we can pass in the
+# writer for the file, in the main we can make the header and comments for the text file and then the function itself
+# is what takes the information)
