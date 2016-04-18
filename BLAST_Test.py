50af081cf3e4cd0e9a152462210bfd2645c436ac
fcocjin <fcocjin@luc.edu>
2016-04-18 15:01:51 -0500

    So I have not fully tested this yet, but I think that this includes a way for the user to input their own cutoffs for query coverage and percent identity. I will work on it today during class and later tonight to verify that it works. I also removed the functionality to create a custom output file name because I feel as though a standardized extension to the name would be best for each different output we create

--- BLAST_Test.py
+++ BLAST_Test.py
@@ -10,35 +10,35 @@ from Bio import SeqIO
 import os
 
 
-def parsefile(blast_record, writer, query_ID):  # This function writes the desired information to the final file
+def parsefile(blast_record, writer, query_ID, percent_identity, query_coverage):  # This function writes the desired information to the final file
     if len(blast_record.alignments) != 0: # Prevents an error from occurring if there is no BLAST match
         alignment = blast_record.alignments[0] # Retrieves the best hit (first alignment)
         for hsp in alignment.hsps: # Retrieves all locations in the best hit
-            # coverage = float(float(hsp.align_length)/float(blast_record.query_length))
-            # if (hsp.identities >= percent_identity): # and (hsp.query_start != hsp.sbjct_start): and
-            # (coverage >= query_coverage)
-            # print(coverage)
-            s = alignment.title
-            l = s.split("|")
-            s = l[2]
-            l1 = s.split(" ")
-            s = l1[2] + ' ' + l1[3] # Finalized organism name
-            writer.write(query_ID + ',' + s + ',' + str(hsp.query_start) + ',' + str(hsp.query_end) + ',' +
-                         str(hsp.sbjct_start) + ',' + str(hsp.sbjct_end) + '\n')
+            coverage = float(float(hsp.align_length)/float(blast_record.query_length))  # Calculates query coverage
+            p_identity = float(float(hsp.identities)/float(hsp.align_length))  # Calculates percent identity
+            if (p_identity >= percent_identity) and ((hsp.query_start != hsp.sbjct_start) and
+                (coverage >= query_coverage)):
+                s = alignment.title
+                l = s.split("|")
+                s = l[2]
+                l1 = s.split(" ")
+                s = l1[2] + ' ' + l1[3] # Finalized organism name
+                writer.write(query_ID + ',' + s + ',' + str(hsp.query_start) + ',' + str(hsp.query_end) + ',' +
+                             str(hsp.sbjct_start) + ',' + str(hsp.sbjct_end) + '\n')
             # Writes the location of the subject hit
 
 
 def main():
     
     query = input('Enter query file name: ')  # For the working example, type in 'gg_pre_mirna_short.fasta'
-    filename = input('What is your desired file name for the top hits file? ')  # 'gg_pre_mirna_short_results.fasta
-    # query_coverage = input('What is your ideal cutoff query coverage?')  # I will use this later to filter results
-    # percent_identity = input('What is your ideal cutoff percent identity')  # same as above
+    locationsFilename = query[0:len(query)-6] + '_locations' + query[len(query)-6:len(query)]
+    query_coverage = input('What is your ideal cutoff query coverage?')  # I will use this later to filter results
+    percent_identity = input('What is your ideal cutoff percent identity')  # same as above
     records = SeqIO.parse(query,'fasta')  # Retrieves miRNA ID's
     queryList = []  # Stores miRNA ID's
     for record in records:
         queryList.append(record.id)
-    writer = open(filename, 'w')
+    writer = open(locationsFilename, 'w')
     writer.write('query_miRNA_ID' + ',' 'Organism_name' + ',' + 'Query_start' + ',' + 'Query_end' + ',' +
                  'Subject_start' + ',' + 'Subject_end' + '\n')  # Writes the header for the results file
     print('Now BLASTing')
@@ -49,7 +49,7 @@ def main():
     blast_records = NCBIXML.parse(result_handle)
     recordCount = 0
     for blast_record in blast_records: # Writes a result for each miRNA BLASTed
-        parsefile(blast_record,writer,queryList[recordCount])
+        parsefile(blast_record,writer,queryList[recordCount], percent_identity, query_coverage)
         recordCount += 1
     writer.close()
     print('Finished!')
