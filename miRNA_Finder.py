57a48889aca8f4311acededf08af7c0815522a2b
fcocjin <fcocjin@luc.edu>
2016-05-05 19:20:32 -0500

    Changed the default qcoverage and percent identity while adding an evalue filter

--- miRNA_Finder.py
+++ miRNA_Finder.py
@@ -25,7 +25,7 @@ def locationParser(file_handle, working_list, organism_name):
     writer = open(file_handle + '_' + organism_name + '_locations.txt', 'w')  # Creates the uniform file extension
     writer.write('Query_miRNA_ID' + ',' + 'Subject_ID' + ',' + 'Organism_name' + ',' + 'Query_start' + ',' +
                  'Query_end' + ',' + 'Subject_start' + ',' +
-                 'Subject_end' + ',' + 'Percent_Identity' + ',' + 'Query_Coverage' +
+                 'Subject_end' + ',' + 'Percent_Identity' + ',' + 'Query_Coverage' + ',' + 'E-Value' +
                  '\n')  # Writes the header for the location results file
     for s in working_list:
         writer.write(s + '\n')
@@ -48,22 +48,28 @@ def main():
     query = raw_input('Enter query file name: ')  # For the working example, type in 'gg_pre_mirna_short.fasta'
     file_handle = query[0:len(query)-6]  # Stores the original file name that we can add extension file names from
     temp_string = raw_input('What is your ideal cutoff query coverage?: ')
-    if temp_string == '':
-        temp_string = '80.0'
-        print('Since no value was inputted, BLAST will use the default query coverage of 80%')
-    query_coverage = float(temp_string)  # The user can input a query coverage.
-    # Amani can add default parameters in the GUI for both query_coverage and percent identity
+    if temp_string != '':
+        query_coverage = float(temp_string)  # The user can input a query coverage.
+    else:
+        print('Since no value was inputted, BLAST will not filter by query coverage')
+        query_coverage = 0
     temp_string = raw_input('What is your ideal cutoff percent identity?: ')
-    if temp_string == '':
-        temp_string = '100.00'
-        print('Since no value was inputted, BLAST will use the default percent identity of 100%')
-    percent_identity = float(temp_string)  # The user can input a percent identity cutoff
+    if temp_string != '':
+        percent_identity = float(temp_string)  # The user can input a percent identity cutoff
+    else:
+        print('Since no value was inputted, BLAST not filter by percent identity')
+        percent_identity = 0
+    temp_string = raw_input('What is your ideal cutoff E-value')
+    if temp_string != '':
+        expectedvalue = float(temp_string)  # The user can input a percent identity cutoff
+    else:
+        print('Since no value was inputted, BLAST will choose a max E-value of 10')
+        expectedvalue = 10
     organism_name = raw_input("What organism's genome is represented by the BLAST database? (No spaces please): ")
     if ' ' in organism_name:
         organism_name = organism_name.replace(' ', '_')
     print('Now BLASTing')
     blast_result_file = file_handle + '_' + organism_name + '_full_BLAST_result.txt'
-    print('DATABASE = ' + database_name)
     os.system('blastn -task blastn-short -query ' + query + ' -db ' + database_name + ' -out '+ blast_result_file +
               ' -num_threads 8 -outfmt "6 std qcovs" ')  # BLASTs from command line
     print('BLAST completed, now parsing file')
@@ -76,14 +82,15 @@ def main():
             query_id = lineList[0]
             subject_id = lineList[1]
             identity = float(lineList[2])
+            evalue = float(lineList[10])
             qcoverage = float(lineList[12])
             qstart = lineList[6]
             qend = lineList[7]
             sstart = lineList[8]
             send = lineList[9]
-            if identity >= percent_identity and (qcoverage >= query_coverage):  # Filters for quality
+            if identity >= percent_identity and (qcoverage >= query_coverage) and (evalue <= expectedvalue):  # Filters for quality
                 working_list.append(query_id + ',' + subject_id + ',' + organism_name + ',' + qstart + ',' + qend +
-                                   ',' + sstart + ',' + send + ',' + str(identity) + ',' + str(qcoverage))
+                                   ',' + sstart + ',' + send + ',' + str(identity) + ',' + str(qcoverage) + ',' + str(evalue))
                 if query_id in count_dictionary:
                     count_dictionary[query_id] = count_dictionary[query_id]+1
                 else:
